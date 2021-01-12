0e51871f21c9780273f48828987240ce564a81bd
divya <60021385+divs-2020@users.noreply.github.com>
2021-01-12 14:44:35 +0530

    constrainedbody

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
+//namespacing 
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -15,6 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    log6 = new Log(230,180,80,PI/2);
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,7 +38,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain= new Chain(bird.body,log6.body);
+   
 
+   
 }
 
 function draw(){
@@ -61,4 +67,9 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
+
+   // strokeWeight(3);
+  //  line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
